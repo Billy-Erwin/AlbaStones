ea695d61a4b57a54d04c163655deb61188a37d73
billy <billy.c.erwin@gmail.com>
2020-07-24 00:30:27 -0400

    Add main carousel

---
+++ alba-stones/src/app/main-slideshow/main-slideshow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-slideshow',
+  templateUrl: './main-slideshow.component.html',
+  styleUrls: ['./main-slideshow.component.sass']
+})
+export class MainSlideshowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
